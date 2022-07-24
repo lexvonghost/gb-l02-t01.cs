6ab36288f51be7b0f147239dc98ec5edb043591e
lexvonghost <lex_von_tost@mail.ru>
2022-07-24 20:18:43 +0300

    feat: решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трёхзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 99 && n < 1000)
+{
+    int d = n / 10 % 10;
+    Console.WriteLine(d);
+}
+else
+{
+    Console.WriteLine("Введено неверное число, попробуйте ещё раз.");
+}
